4728258b787c2961fb500a2a92993994358361bd
Nakorn Sinpadung <nakorndev@gmail.com>
2021-05-12 14:22:42 +0700

    Fix bug for options.quite

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const https = require('https')
 
 const main = (url, downloadPath, options = {}) => {
     return new Promise((resolve, reject) => {
-        if (options.quite) {
-            Console.prototype.log = () => {}
-        }
-
         const req = https.request(url, (res) => {
             if (!res.headers['content-type'].startsWith('application/')) {
                 return reject(new Error('url is not application/*'))
@@ -14,13 +10,17 @@ const main = (url, downloadPath, options = {}) => {
         
             const writeFile = fs.createWriteStream(downloadPath)
             const fileSize = res.headers['content-length']
-            console.log(`- download... (total file size: ${(fileSize / 1024 / 1024).toFixed(2)}MB)`)
+            if (!options.quite) {
+                console.log(`- download... (total file size: ${(fileSize / 1024 / 1024).toFixed(2)}MB)`)
+            }
         
             let progress = 0
         
             res.on('data', (chunk) => {
                 progress += chunk.length / fileSize
-                console.log(`- download (${(progress * 100).toFixed(2)}%)`)
+                if (!options.quite) {
+                    console.log(`- download (${(progress * 100).toFixed(2)}%)`)
+                }
             })
         
             res.on('end', () => {
